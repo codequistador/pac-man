3582c85b4c2d40d37353c1221feb2685d5219912
Justin Daining <justin@justindaining.com>
2016-11-13 21:52:51 -0700

    4b

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -2,16 +2,19 @@ import Ember from 'ember';
 import KeyboardShortcuts from 'ember-keyboard-shortcuts/mixins/component';
 
 export default Ember.Component.extend(KeyboardShortcuts, {
+
   didInsertElement: function() {
     this.drawCircle();
-  }, 
+  },
 
+  x: 50,
+  y: 100,
+  squareSize: 40,
   drawCircle: function() {
-    let canvas = document.getElementById("myCanvas");
-    let ctx = canvas.getContext("2d");
-    let x = 50;
-    let y = 100;
-    let radius = 20;
+    let ctx = this.get('ctx');
+    let x = this.get('x');
+    let y = this.get('y');
+    let radius = this.get('squareSize')/2;
 
     ctx.fillStyle = '#000';
     ctx.beginPath();
@@ -20,11 +23,31 @@ export default Ember.Component.extend(KeyboardShortcuts, {
     ctx.fill();
   },
 
-  keyboardShortcuts: {
-    up: function() {console.log('up');},
-    down: function() {console.log('down');},
-    left: function() {console.log('left');},
-    right: function() {console.log('right');},
+  clearScreen: function() {
+    let ctx = this.get('ctx');
+    let screenWidth = 800;
+    let screenHeight = 600;
+
+    ctx.clearRect(0, 0, screenWidth, screenHeight);
   },
-  
+
+  movePacMan: function(direction, amount) {
+    this.incrementProperty(direction, amount);
+    this.clearScreen();
+    this.drawCircle();
+  },
+
+  ctx: Ember.computed(function(){
+    let canvas = document.getElementById("myCanvas");
+    let ctx = canvas.getContext("2d");
+    return ctx;
+  }),
+
+  keyboardShortcuts: {
+    up: function() { this.movePacMan('y', -1 * this.get('squareSize')); },
+    down: function() { this.movePacMan('y', this.get('squareSize')); },
+    left: function() { this.movePacMan('x', -1 * this.get('squareSize')); },
+    right: function() { this.movePacMan('x', this.get('squareSize')); },
+  },
+
 });
