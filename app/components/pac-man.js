a52d90a381df466c80856f5ba8899f2a467e5b5e
Justin Daining <justin@justindaining.com>
2016-11-19 15:18:40 -0700

    pellet processor

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -4,8 +4,8 @@ import KeyboardShortcuts from 'ember-keyboard-shortcuts/mixins/component';
 export default Ember.Component.extend(KeyboardShortcuts, {
 
   didInsertElement: function() {
-    this.drawWalls();
-    this.drawCircle();
+    this.drawGrid();
+    this.drawPac();
   },
 
   // Component-level vars
@@ -14,13 +14,14 @@ export default Ember.Component.extend(KeyboardShortcuts, {
   squareSize: 40,
   // 0 is a blank space
   // 1 is a wall
+  // 2 is a pellet
   grid: [
-    [0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 1, 0, 1, 0, 0, 0, 1],
-    [0, 0, 1, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
+    [2, 2, 2, 2, 2, 2, 2, 1],
+    [2, 1, 2, 1, 2, 2, 2, 1],
+    [2, 2, 1, 2, 2, 2, 2, 1],
+    [2, 2, 2, 2, 2, 2, 2, 1],
+    [2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 1],
   ],
   screenWidth: Ember.computed(function() {
     return this.get('grid.firstObject.length');
@@ -30,36 +31,53 @@ export default Ember.Component.extend(KeyboardShortcuts, {
   }),
 
   // Drawing and erasing
-  drawCircle: function() {
-    let ctx = this.get('ctx');
+  drawPac() {
     let x = this.get('x');
     let y = this.get('y');
+    let radiusDivisor = 2;
+    this.drawCircle(x, y, radiusDivisor);
+  },
+
+  drawPellet(x, y) {
+    let radiusDivisor = 6;
+    this.drawCircle(x, y, radiusDivisor);
+  },
+
+  drawCircle: function(x, y, radiusDivisor) {
+    let ctx = this.get('ctx');
     let squareSize = this.get('squareSize');
 
     let pixelX = (x+1/2) * squareSize;
     let pixelY = (y+1/2) * squareSize;
 
-    ctx.fillStyle = '#fff';
+    ctx.fillStyle = '#000';
     ctx.beginPath();
-    ctx.arc(pixelX, pixelY, squareSize/2, 0, Math.PI * 2, false);
+    ctx.arc(pixelX, pixelY, squareSize/radiusDivisor, 0, Math.PI * 2, false);
     ctx.closePath();
     ctx.fill();
   },
 
-  drawWalls: function() {
-    let squareSize = this.get('squareSize');
+  drawWall: function(x, y) {
     let ctx = this.get('ctx');
-    ctx.fillStyle = '#000';
+    let squareSize = this.get('squareSize');
 
+    ctx.fillStyle = '#000';
+    ctx.fillRect(x * squareSize,
+                 y * squareSize,
+                 squareSize,
+                 squareSize);
+  },
+
+  drawGrid: function() {
     let grid = this.get('grid');
-    grid.forEach(function(row, rowIndex){
-      row.forEach(function(cell, columnIndex){
+    grid.forEach((row, rowIndex)=>{
+      row.forEach((cell, columnIndex)=>{
         if(cell === 1){
-          ctx.fillRect(columnIndex * squareSize,
-                       rowIndex * squareSize,
-                       squareSize,
-                       squareSize);
+          this.drawWall(columnIndex, rowIndex);
         } 
+        if(cell === 2){
+          this.drawPellet(columnIndex, rowIndex);
+        }
       });
     });
   },
@@ -78,9 +96,21 @@ export default Ember.Component.extend(KeyboardShortcuts, {
       this.decrementProperty(direction, amount);
     }
 
+    this.processAnyPellets();
+
     this.clearScreen();
-    this.drawWalls();
-    this.drawCircle();
+    this.drawGrid();
+    this.drawPac();
+  },
+
+  processAnyPellets: function() {
+    let x = this.get('x');
+    let y = this.get('y');
+    let grid = this.get('grid');
+
+    if(grid[y][x] === 2) {
+      grid[y][x] = 0;
+    }
   },
 
   collidedWithBorder: function() {
